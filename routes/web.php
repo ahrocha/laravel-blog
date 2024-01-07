fb3100e4ad68019af9362dedb807c6f0b2679b37
Andrey Rocha <ar@tsl.io>
2024-01-07 16:56:38 -0300

    Switch posts endpoint to root

--- routes/web.php
+++ routes/web.php
@@ -15,8 +15,9 @@ use App\Http\Controllers\PostController;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
-Route::resource('posts', PostController::class);
+Route::resource('/', PostController::class)->names([
+    'index' => 'posts.index',
+    'create' => 'posts.create',
+    'store' => 'posts.store',
+    'show' => 'posts.show',
+  ]);
